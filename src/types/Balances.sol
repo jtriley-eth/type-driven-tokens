42203c40a27b88813c18d79b8801fd565706fa19
jtriley.eth <Jtriley15@gmail.com>
2024-01-18 20:44:41 -0600

    add write

--- src/types/Balances.sol
+++ src/types/Balances.sol
@@ -5,10 +5,15 @@ struct Balances {
     mapping(address => uint256) _inner;
 }
 
-using { read, increase, decrease } for Balances global;
+using { read, write, increase, decrease } for Balances global;
 
 function read(Balances storage self, address account) view returns (uint256) {
     return self._inner[account];
+}
+
+function write(Balances storage self, address account, uint256 amount) returns (Balances storage) {
+    self._inner[account] = amount;
+    return self;
 }
 
 function increase(
