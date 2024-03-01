5b16a02d33400666208690bb759e72cab155f5a9
Carter Carlson <pegahcarter@gmail.com>
2024-03-01 13:00:45 -0800

    feat: upgradeable storage

--- src/ERC20.sol
+++ src/ERC20.sol
@@ -7,34 +7,40 @@ contract ERC20 {
     event Transfer(address indexed from, address indexed to, uint256 value);
     event Approval(address indexed owner, address indexed spender, uint256 value);
 
-    Token internal self;
+    // bytes32 public constant STORAGE_SLOT = bytes32(uint256(keccak256('type-driven-tokens.storage.ERC20')) - 1);
+    bytes32 public constant STORAGE_SLOT = 0x8787a9118c1e5905a458ef79e731baeec99367fa665f097b8495d5d64cde7e94;
+    function self() private pure returns (Token storage $) {
+        assembly {
+            $.slot := STORAGE_SLOT
+        }
+    }
 
     function totalSupply() public view returns (uint256) {
-        return self.totalSupply();
+        return self().totalSupply();
     }
 
     function balanceOf(address account) public view returns (uint256) {
-        return self.balances.read(account);
+        return self().balances.read(account);
     }
 
     function allowance(address owner, address spender) public view returns (uint256) {
-        return self.allowances.read(owner, spender);
+        return self().allowances.read(owner, spender);
     }
 
     function transfer(address receiver, uint256 amount) public returns (bool) {
-        self.transfer(msg.sender, receiver, amount);
+        self().transfer(msg.sender, receiver, amount);
         emit Transfer(msg.sender, receiver, amount);
         return true;
     }
 
     function transferFrom(address sender, address receiver, uint256 amount) public returns (bool) {
-        self.transferFrom(msg.sender, sender, receiver, amount);
+        self().transferFrom(msg.sender, sender, receiver, amount);
         emit Transfer(sender, receiver, amount);
         return true;
     }
 
     function approve(address spender, uint256 amount) public returns (bool) {
-        self.approve(msg.sender, spender, amount);
+        self().approve(msg.sender, spender, amount);
         emit Approval(msg.sender, spender, amount);
         return true;
     }
--- src/ERC6909.sol
+++ src/ERC6909.sol
@@ -2,11 +2,6 @@
 pragma solidity 0.8.22;
 
 import { MultiToken } from "./types/MultiToken.sol";
-import { Operators } from "./types/Operators.sol";
-
-struct ERC6909Store {
-    MultiToken multiToken;
-}
 
 contract ERC6909 {
     event Transfer(
@@ -17,26 +12,32 @@ contract ERC6909 {
     );
     event OperatorSet(address indexed owner, address indexed operator, bool approved);
 
-    ERC6909Store internal self;
+    // bytes32 public constant STORAGE_SLOT = bytes32(uint256(keccak256('type-driven-tokens.storage.ERC6909)) - 1);
+    bytes32 public constant STORAGE_SLOT = 0x894b575d503a5e7d146da3671583044031de353234c66e814bda6299a55ad8e6;
+    function self() private pure returns (MultiToken storage $) {
+        assembly {
+            $.slot := STORAGE_SLOT
+        }
+    }
 
     function totalSupply(uint256 id) public view returns (uint256) {
-        return self.multiToken.totalSupply(id);
+        return self().totalSupply(id);
     }
 
     function balanceOf(address owner, uint256 id) public view returns (uint256) {
-        return self.multiToken.balanceOf(id, owner);
+        return self().balanceOf(id, owner);
     }
 
     function allowance(address owner, address spender, uint256 id) public view returns (uint256) {
-        return self.multiToken.allowance(id, owner, spender);
+        return self().allowance(id, owner, spender);
     }
 
     function isOperator(address owner, address operator) public view returns (bool) {
-        return self.multiToken.isOperator(owner, operator);
+        return self().isOperator(owner, operator);
     }
 
     function transfer(address to, uint256 id, uint256 amount) public returns (bool) {
-        self.multiToken.transfer(id, msg.sender, to, amount);
+        self().transfer(id, msg.sender, to, amount);
         emit Transfer(msg.sender, msg.sender, to, id, amount);
         return true;
     }
@@ -47,19 +48,19 @@ contract ERC6909 {
         uint256 id,
         uint256 amount
     ) public returns (bool) {
-        self.multiToken.transferFrom(id, msg.sender, from, to, amount);
+        self().transferFrom(id, msg.sender, from, to, amount);
         emit Transfer(msg.sender, from, to, id, amount);
         return true;
     }
 
     function approve(address spender, uint256 id, uint256 amount) public returns (bool) {
-        self.multiToken.approve(id, msg.sender, spender, amount);
+        self().approve(id, msg.sender, spender, amount);
         emit Approval(msg.sender, spender, id, true);
         return true;
     }
 
     function setOperator(address operator, bool approved) public returns (bool) {
-        self.multiToken.setOperator(msg.sender, operator, approved);
+        self().setOperator(msg.sender, operator, approved);
         emit OperatorSet(msg.sender, operator, approved);
         return true;
     }
